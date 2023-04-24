5a1d431dc4a388aac0dd2a7233047a663d699e3c
dannyEnagu <danielEnagu@gmail.com>
2023-04-24 13:12:46 +0000

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Returns information about his/her TODO list progress.
+   The module Takes in a user id, sends a request to an API
+   and displays the response to standard output using python
+   response package.
+"""
+import sys
+import requests
+
+
+URL = "https://jsonplaceholder.typicode.com"
+"""Link to API"""
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) > 1 and
+            isinstance(int(sys.argv[1]), int)):
+        user_id = int(sys.argv[1])
+        user = requests.get("{}/users/{}".format(URL, user_id))
+        todos = requests.get("{}/user/{}/todos/".format(URL, user_id))
+        if (user.status_code == requests.codes.ok and
+                todos.status_code == requests.codes.ok):
+            username = user.json().get('name')
+            done_tasks = []  # List of done/completed tasks
+            c_tasks = 0      # Number of completed tasks
+            tasks = 0        # Total number of tasks
+            for todo in todos.json():
+                task_status = todo.get('completed')
+                if task_status is True:
+                    c_tasks = c_tasks + 1
+                    done_tasks.append(todo)
+                tasks = tasks + 1
+            print("Employee {} is done with tasks({}/{}):".format(
+                   username,
+                   c_tasks,
+                   tasks))
+            for todo in done_tasks:
+                print("\t {}".format(todo.get('title')))
