2561eb07943de376e355af6750d90ffd1604de33
dannyEnagu <danielEnagu@gmail.com>
2023-05-18 16:30:05 +0000

    1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Change the OS configuration so that it is possible to
+# login with the holberton user and open a file without
+# any error message.
+
+exec { 'Change the OS configuration':
+  command => "bash -c \"sed -iE 's/^holberton hard nofile \
+5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+sed -iE 's/^holberton soft nofile \
+4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
